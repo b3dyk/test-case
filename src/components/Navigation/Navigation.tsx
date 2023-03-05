954398adbb0071373783039341576960f18fa9a4
Oleksandr Perlov <oleksandrperlov@gmail.com>
2023-03-05 22:37:44 +0200

    Update Navigation.tsx

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -3,9 +3,12 @@ import icon from "../../images/logo.png";
 import { useTranslation } from "react-i18next";
 
 import { Item, List, NavBar, StyledLink } from "./Navigation.styled";
+import { useAppSelector } from "../../redux/hooks";
+import { selectUser } from "../../redux/auth/auth.selector";
 
 export const Navigation: React.FC = () => {
   const { t } = useTranslation();
+  const user = useAppSelector(selectUser);
 
   return (
     <NavBar>
@@ -20,9 +23,11 @@ export const Navigation: React.FC = () => {
         <Item>
           <StyledLink to="/news">{t("news")}</StyledLink>
         </Item>
-        <Item>
-          <StyledLink to="/profile">{t("profile")}</StyledLink>
-        </Item>
+        {user && (
+          <Item>
+            <StyledLink to="/profile">{t("profile")}</StyledLink>
+          </Item>
+        )}
       </List>
     </NavBar>
   );
